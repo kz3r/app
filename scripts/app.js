027f22c0ec9a4af904a62265a958cc86ad480327
orzechowski <alinedaisy@gmail.com>
2016-11-23 23:50:37 -0200

    okk

--- scripts/app.js
+++ scripts/app.js
@@ -36,7 +36,10 @@ angular
                     'scripts/directives/header/header.js',
                     'scripts/directives/header/header-notification/header-notification.js',
                     'scripts/directives/sidebar/sidebar.js',
-                    'scripts/directives/sidebar/sidebar-search/sidebar-search.js'
+                    'scripts/directives/sidebar/sidebar-search/sidebar-search.js',
+						  'shared/genseqAutenticacao/autenticacaoService.js',
+                    'components/genseqLogin/loginService.js',
+                    'shared/genseqNavegacao/navController.js'
                     ]
                 }),
                 $ocLazyLoad.load(
@@ -46,39 +49,7 @@ angular
                           "bower_components/angular-toggle-switch/angular-toggle-switch.css"
                       ]
                 })
-                $ocLazyLoad.load({
-                    name:'sbAdminApp',
-                    files:[
-                      'shared/genseqAutenticacao/autenticacaoService.js',
-                      'components/genseqLogin/loginService.js',
-                      'shared/genseqNavegacao/navController.js'
-                    ]
-                })
-                $ocLazyLoad.load(
-                {
-                  name:'ngAnimate',
-                  files:['bower_components/angular-animate/angular-animate.js']
-                })
-                $ocLazyLoad.load(
-                {
-                  name:'ngCookies',
-                  files:['bower_components/angular-cookies/angular-cookies.js']
-                })
-                $ocLazyLoad.load(
-                {
-                  name:'ngResource',
-                  files:['bower_components/angular-resource/angular-resource.js']
-                })
-                $ocLazyLoad.load(
-                {
-                  name:'ngSanitize',
-                  files:['bower_components/angular-sanitize/angular-sanitize.js']
-                })
-                $ocLazyLoad.load(
-                {
-                  name:'ngTouch',
-                  files:['bower_components/angular-touch/angular-touch.js']
-                })
+
             }
         }
     })
